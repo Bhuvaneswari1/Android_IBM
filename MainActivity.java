2a3abff06231ec0b7d7914dbcd11b8e8c2e9cec1
Bhuvaneswari1 <50100445+Bhuvaneswari1@users.noreply.github.com>
2022-07-15 16:45:07 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.progressbar_example2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+    int progress = 0;
+    ProgressBar simpleProgressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        simpleProgressBar = (ProgressBar) findViewById(R.id.simpleProgressBar);
+        Button startButton = (Button) findViewById(R.id.startButton);
+        // perform click event on button
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // call a function
+                setProgressValue(progress);
+
+            }
+        });
+    }
+
+    private void setProgressValue(final int progress) {
+
+        // set the progress
+        simpleProgressBar.setProgress(progress);
+        // thread is used to change the progress value
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                setProgressValue(progress + 10);
+            }
+        });
+        thread.start();
+    }
+}
